9148a553ddae89b91c89b1f90d50d88e2c4be6dc
pammi7645 <pammi99310lry@gmail.com>
2024-04-25 20:47:44 +0530

    23march

---
+++ PRACTICE QUESTION/sizeoftypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    bool b;
+    float c;
+    cout<<"size of int"<<endl;
+    cout<<"size of bool"<<endl;
+    cout<<"size of float"<<endl;
+    return 0;
+}
