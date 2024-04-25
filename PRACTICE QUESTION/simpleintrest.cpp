9c0d4fe7351179c0f89df22c527150fc462ad072
pammi7645 <pammi99310lry@gmail.com>
2024-04-25 20:34:59 +0530

    23march

---
+++ PRACTICE QUESTION/simpleintrest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float si,p,t,r;
+    cout<<"value of p";
+    cin>>p;
+    cout<<"value of t";
+    cin>>t;
+    cout<<"value of r";
+    cin>>r;
+    si=(p*t*r)/100;
+cout<<"si is:"<<"si";
+    return 0;
+}
