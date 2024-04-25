eb278f3e63d777b2cec5dbd97dd86311e02a4c29
pammi7645 <pammi99310lry@gmail.com>
2024-04-25 21:13:43 +0530

    23march

---
+++ PRACTICE QUESTION/circleofcircumfrence.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float circumfrence, r=6;
+    circumfrence=2*3.14*r;
+    cout<<"circumfrence of circle is:"<<circumfrence;
+    return 0;
+}
