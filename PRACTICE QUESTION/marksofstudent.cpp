800d77e4e1e640ab2bff32a8259858a67c975a64
pammi7645 <pammi99310lry@gmail.com>
2024-04-27 22:10:17 +0530

    24march

---
+++ PRACTICE QUESTION/marksofstudent.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cin>>marks;
+    if(marks > 50)
+    cout<<"pass"<<endl;
+else{
+    cout<<"fail";
+    return 0;
+}
+};
