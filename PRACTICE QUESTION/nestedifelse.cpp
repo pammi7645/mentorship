338e95037a24e0936f481530a9fc70281cab4c3f
pammi7645 <pammi99310lry@gmail.com>
2024-04-27 22:54:48 +0530

    24march

---
+++ PRACTICE QUESTION/nestedifelse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    if(marks >=80)
+    cout<<"gressfully pass"<<endl;
+        else{
+            cout<<"fail";
+            return 0;
+            }
+        };
