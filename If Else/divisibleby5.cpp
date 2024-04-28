ce3cf8a0296cbd5631e80d31e395dc7c6bb57d74
pammi7645 <pammi99310lry@gmail.com>
2024-04-28 18:50:47 +0530

    24march

---
+++ If Else/divisibleby5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter  the number: ";
+    cin>>num;
+    if(num%5==0)
+    cout<<"number is divisible";
+    else
+{
+    cout<<"not divisible";
+}
+return 0;
+}
