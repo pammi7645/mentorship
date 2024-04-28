31cdbc968279557515ca5bc4116d0b824aabad4e
pammi7645 <pammi99310lry@gmail.com>
2024-04-28 18:46:52 +0530

    24march

---
+++ If Else/threedigitnumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+cout<<"inter the value of a: ";
+cin>>a;
+if((a<1000) && (a>99))
+{
+cout<<"is the three digit number";
+}
+else
+{
+    cout<<"number is not three digit";
+}
+ return 0;
+}
