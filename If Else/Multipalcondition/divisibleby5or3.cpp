c729374e78879fd716cd76c5c381c7d9c2e55a49
pammi7645 <pammi99310lry@gmail.com>
2024-04-28 19:12:50 +0530

    24march

---
+++ If Else/Multipalcondition/divisibleby5or3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the value of a :";
+    cin>>a;
+    if ((a%5==0) || (a%3==0))
+    {
+    cout<<"it is divisilble";
+    }
+    else
+    {
+        cout<<"it is not divisible ";
+    }
+        return 0;
+    }
+
+  
