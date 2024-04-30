608e99a6dbb9fa15fe2665e30b68d1eb33fa3815
pammi7645 <pammi99310lry@gmail.com>
2024-05-01 00:25:42 +0530

    29march

---
+++ LOOPS.CPP/oddnumber1to100.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;i<=99;i=i+2)
+    {
+    cout<<i<<endl;
+}
+return 0;
+}
---
+++ LOOPS.CPP/tableofn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"value of n:";
+    cin>>n;
+    for(i=1;i<=10;i++)
+    {
+        cout<<n*1<<endl;
+    }
+    return 0;
+}
